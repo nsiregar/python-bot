4b76eab99eaea05fe06571b6fa933460431837ef
Ngalim Siregar <ngalim.siregar@gmail.com>
2016-05-31 04:33:36 +0900

    Add basic function for Bing Search API

---
+++ modules/bing.py
@@ -0,0 +1,21 @@
+import urllib
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+
+API_KEY = "0tRJGHkAOqn8mNuifoi5dcdXnq550Ov/mt2pA6C5544"
+
+def bing_search(query, source_type = "Web", top = 1, format = 'json'):
+	query = '%27' + urllib.parse.quote(query) + '%27'
+	base_url = 'https://api.datamarket.azure.com/Bing/SearchWeb/v1/' + source_type
+	url = base_url + '?Query=' + query + '&$top=' + str(top) + '&format=' + format
+
+	user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36"
+	auth = HTTPBasicAuth(API_KEY, API_KEY)
+	headers = {'User-Agent': user_agent}
+
+	response_data = requests.get(url, headers = headers, auth = auth)
+	json_result = response_data.json()
+
+	return json_result
+
